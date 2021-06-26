4f1754a0bc27e3a27391d6d3a07e5c17ed3fe33d
songqian2018 <921235158@qq.com>
2021-06-26 13:33:47 +0800

    update test

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -4,7 +4,7 @@
 
             <div class="banner">
                 <div class="container">
-                <h1 class="logo-font">conduit</h1>
+                <h1 class="logo-font">热血传奇</h1>
                 <p>A place to share your knowledge.</p>
                 </div>
             </div>
